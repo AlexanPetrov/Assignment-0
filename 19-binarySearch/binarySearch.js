92476fced96fc5fa02ec117d49e87fefcf4b24a7
Alexander Petrovski <apfb11@gmail.com>
2020-12-22 11:53:50 -0500

    corrections on q-19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,25 +1,25 @@
 /* recursive option of binary search */
 
 class MySolution {
-    recursiveBinarySearch(n, arr) {
-        let mid = Math.floor(arr.length / 2);
-        if (arr.length === 1 && arr[0] != n) {
+    binarySearch(nums, target) {
+        let mid = Math.floor(nums.length / 2);
+        if (nums.length === 1 && nums[0] != target) {
             return false;
         }
-        if (n === arr[mid]) {
+        if (target === nums[mid]) {
             return true;
         }
-        else if (n < arr[mid]) {
-            return this.recursiveBinarySearch(n, arr.slice(0, mid));
+        else if (target < nums[mid]) {
+            return this.binarySearch(nums.slice(0, mid), target);
         }
-        else if (n > arr[mid]) {
-            return this.recursiveBinarySearch(n, arr.slice(mid));    
+        else if (target > nums[mid]) {
+            return this.binarySearch(nums.slice(mid), target);    
         }
     }
 }
  
 let ms = new MySolution();
-console.log(ms.recursiveBinarySearch(5, [1,2,3,4,5]));
+console.log(ms.binarySearch([1,2,3,4,5], 5));
 
 // Do not edit this line;
 let StudentSolution = MySolution;
